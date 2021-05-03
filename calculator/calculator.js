247e0377d57c1c2cbff9a91e08c09ec962524551
Nisarg <nisarg38@gmail.com>
2021-05-04 00:04:38 +0530

    2nd Project updated

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,5 +1,31 @@
+const { symbol, number } = require("prop-types");
+
 let total = 0;
 let buffer = "0";
 let prevoperator; 
 const screen = document.querySelector(".screen");
 
+function init(){
+  document.querySelector(".button").addEventListener("click", buttonClick(event.target.innerText));
+}
+
+function render() {
+  screen.innerText = buffer;
+}
+
+function buttonClick(val){
+  if(isNaN(val)){
+    symbol(val);
+  }else{
+    number(val);
+  }
+}
+
+function number(val){
+  
+}
+
+
+
+
+init();
